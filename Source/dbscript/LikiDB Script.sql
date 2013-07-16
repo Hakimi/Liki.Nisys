2b281544b8edde7307f34cab1eb1dab04685cb55
Tarun <tarun.jadav@gmail.com>
2013-07-16 18:29:59 +0530

    remove customer table and added user table

--- Source/dbscript/LikiDB Script.sql
+++ Source/dbscript/LikiDB Script.sql
@@ -4,6 +4,8 @@ GO
 create database LikiDB
 Go
 
+USE [LikiDB]
+GO
  
 /****** Object:  Table [dbo].[Customer]    Script Date: 07/15/2013 22:28:50 ******/
 SET ANSI_NULLS ON
@@ -12,21 +14,36 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_PADDING ON
 GO
-CREATE TABLE [dbo].[Customer](
-	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
-	[EmailAddress] [varchar](70) NOT NULL,
-	[Title] [varchar](20) NOT NULL,
-	[FirstName] [varchar](30) NOT NULL,
-	[MiddleName] [varchar](30) NOT NULL,
-	[LastName] [varchar](30) NOT NULL,
-	[Suffix] [varchar](10) NOT NULL,
-	[Password] [varchar](200) NOT NULL,
-	[RoleID] [int] NULL,
- CONSTRAINT [Customer_PK] PRIMARY KEY CLUSTERED 
-(
-	[CustomerID] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY]
+CREATE TABLE Users(
+
+      UserID int IDENTITY(1,1) NOT NULL,
+
+      UserFID varchar(15) NOT NULL,
+
+      EmailAddress varchar(100) NOT NULL,
+
+      Title varchar(15) NULL,
+
+      FirstName varchar(50) NOT NULL,
+
+      MiddleName varchar(50) NULL,
+
+      LastName varchar(50) NULL,
+
+      Suffix varchar(15) NULL,
+
+      Password varchar(200) NOT NULL,
+
+      MobileNo varchar(15) NULL,
+
+      SSN char(9) NULL,
+
+      DateOfBirth date NULL,
+
+      RoleID int NOT NULL DEFAULT(0),
+
+CONSTRAINT User_PK PRIMARY KEY CLUSTERED ( UserID ASC ) )
+
 GO
 SET ANSI_PADDING OFF
 GO
